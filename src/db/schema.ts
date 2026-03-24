b32e697c264e0134c7df99b5e384d367d847ba96
realm2100 <kur2100a@gmail.com>
2026-03-24 16:19:58 +0900

    add bot config schema for future

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,16 +1,22 @@
 import {
+  check,
   integer,
   sqliteTable,
   text,
 } from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
 
+const botConfigSchema = sqliteTable("bot_config", {
+  id: integer("id").primaryKey().default(1),
+  logChannelId: text("log_channel_id").notNull(),
+}, (table) => [
+  check("bot_config", sql`${table.id} = 1`),
+]);
+
 const anonymousMessagesSchema = sqliteTable("anonymous_messages", {
   authorId: text("author_id").notNull(),
-  createdAt: integer("created_at", {
-    mode: "timestamp_ms",
-  }).notNull().default(sql`(unixepoch() * 1000)`),
+  createdAt: integer("created_at").notNull().default(sql`(unixepoch() * 1000)`),
   messageId: text("message_id").primaryKey(),
 });
 
-export { anonymousMessagesSchema };
+export { anonymousMessagesSchema, botConfigSchema };
